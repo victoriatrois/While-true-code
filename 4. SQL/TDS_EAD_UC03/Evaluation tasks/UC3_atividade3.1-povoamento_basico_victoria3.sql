c6b817a2040bf89dc189a94fd0a40d24deba4543
Victoria Trois <v3gc@hotmail.com>
2022-04-23 17:37:13 -0300

    Database population practice
    After some time struggling with an FK, job's done o/

--- 4. SQL/TDS_EAD_UC03/Evaluation tasks/UC3_atividade3.1-povoamento_basico_victoria3.sql
+++ 4. SQL/TDS_EAD_UC03/Evaluation tasks/UC3_atividade3.1-povoamento_basico_victoria3.sql
@@ -1,14 +1,14 @@
- /*script de povoamento básico para bom funcionamento do BD*/
+ -- script de povoamento básico para bom funcionamento do BD
  insert into TIPO_QUARTO values
- ('1', 'apartamento'),
- ('2', 'quartos duplos'),
- ('3', 'enfermaria');
+ ('1', 'apartamento', '1500'),
+ ('2', 'quartos duplos', '800'),
+ ('3', 'enfermaria', '500');
  
   insert into CONVENIO values
- ('1', 'Unimed', '87096616000196', '6 meses'),
- ('2', 'Agemed', '02933220000101', 'sem carência'),
- ('3', 'Cassi', '33719485000399', '3 meses'),
- ('4', 'Ipê', '12132095000186', '4 meses');
+ ('10', 'Unimed', '87096616000196', '6 meses'),
+ ('20', 'Agemed', '02933220000101', 'sem carência'),
+ ('30', 'Cassi', '33719485000399', '3 meses'),
+ ('40', 'Ipê', '12132095000186', '4 meses');
  
   insert into CATEGORIA values
  ('1','pediatria'),
