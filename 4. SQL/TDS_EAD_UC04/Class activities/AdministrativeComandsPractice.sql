e120d358d335382a09f9a77ebd5a5ac5798ebb99
Victoria Trois <vickytrois@outlook.com>
2022-06-02 11:11:36 -0300

    DB studies
    SQL management commands

---
+++ 4. SQL/TDS_EAD_UC04/Class activities/AdministrativeComandsPractice.sql
@@ -0,0 +1,106 @@
+-- Criando um usuário teste
+create user 'teste'@'localhost' identified by 'q1w2e3r4';
+
+-- Selecionando todos os dados dos usuários existentes no servidor (confirmando a criação do usuário teste)
+SELECT 
+    *
+FROM
+    mysql.user;
+
+-- Selecionando apenas o host e o nome de usuário dos usuários existentes
+SELECT 
+    host, user
+FROM
+    mysql.user;
+
+create user 'vicky3'@'localhost' identified by '@Z03yS2mySQL';
+
+-- Concedendo todas as permissões possíveis a um usuário
+grant all privileges on *.* to 'vicky3'@'localhost';
+
+-- Atualizando alterações de concessões concedidas ou revogadas
+flush privileges;
+
+drop user 'teste'@'localhost';
+
+create user 'teste2'@'localhost' identified by 'test&2';
+
+-- Deletando um usuário
+drop user 'teste2'@'localhost';
+
+-- Criando um usuário
+create user 'dimi'@'localhost' identified by 'd1m1@';
+
+-- Concedendo permissões a um usuário
+grant select, insert, update, create, drop on *.* to 'dimi'@'localhost';
+
+-- Revogando permissões a um usuário
+revoke delete, alter on *.* from 'dimi'@'localhost';
+
+-- Mostrando as permissões concedidas a um usuário
+show grants for 'dimi'@'localhost';
+
+show grants for 'vicky3'@'localhost';
+
+-- Criando papéis
+create role 'developer', 'read_only', 'write_allowed';
+
+create database app_db;
+
+use app_db;
+
+-- Concedendo permissões a papeis
+grant all on app_db.* to 'developer';
+grant select on app_db.* to 'read_only';
+grant insert, update, delete on app_db.* to 'write_allowed';
+
+-- criando usuários
+create user 'developer_user'@'localhost';
+create user 'only_read_user'@'localhost';
+create user 'write_allowed_user'@'localhost';
+
+-- Designando papéis a usuários
+grant 'developer' to 'developer_user'@'localhost';
+grant 'read_only' to 'only_read_user'@'localhost';
+grant 'write_allowed' to 'write_allowed_user'@'localhost';
+
+-- Criando tabela com PK autoincrementavel
+CREATE TABLE cadastro_usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(30),
+    senha TEXT
+);
+
+-- Como não cadastrar senhas (elas estarão expostas dessa forma)
+insert into cadastro_usuarios (usuario, senha) values ('Usuário Normal', 'SenacEAD_2022');
+DELETE FROM cadastro_usuarios 
+WHERE
+    id = 1;
+-- Encriptografando senhas com md5
+insert into cadastro_usuarios (usuario, senha) values ('usuario md5', md5('SenacEAD_2022'));
+
+-- Encriptografando senhas com sha1
+INSERT INTO cadastro_usuarios (usuario, senha) VALUES ('Usuário SHA-1', sha1('SenacEAD_2022'));
+
+-- Encriptografando senhas com sha2
+INSERT INTO cadastro_usuarios (usuario, senha) VALUES ('Usuário SHA-2 ', sha2('SenacEAD_2022', 224));
+
+ALTER TABLE cadastro_usuarios ADD COLUMN senha_aes varbinary(100);
+
+-- Encriptografando senhas com AES
+INSERT INTO cadastro_usuarios (usuario, senha_aes) VALUES ('Usuário AES', AES_ENCRYPT('SenacEAD_2022', 'Minha senha secreta' ));
+
+select * from cadastro_usuarios;
+
+-- Descriptografando senhas AES (dessa forma ela vai retornar um BLOB)
+SELECT
+AES_DECRYPT(senha_aes, 'Minha senha secreta')
+FROM cadastro_usuarios
+WHERE senha_aes is not null;
+
+-- Convertendo um BLOB decriptografado em uma STRING
+SELECT
+CAST(AES_DECRYPT(senha_aes, 'Minha senha secreta') AS CHAR(255))
+FROM cadastro_usuarios
+WHERE senha_aes is not null;
+
---
+++ 4. SQL/TDS_EAD_UC04/Evaluation tasks/UC4_atividade1_victoria3.sql
@@ -0,0 +1,29 @@
+-- Crie um usuário específico para relatórios. Crie role para ele, com acesso apenas à consulta em tabelas (nem dados, nem estrutura podem ser alterados).
+CREATE ROLE 'consulta';
+
+GRANT SELECT ON uc4atividades.* TO consulta;
+
+CREATE USER 'dimi.machado'@'localhost';
+
+GRANT 'consulta' TO 'dimi.machado'@'localhost';
+
+-- 	• Crie usuário e role para funcionário, o qual pode manipular as tabelas (as tabelas ou os dados?) de venda, cliente e produto, mas não deve ter acesso (nem para consulta) a funcionário e cargo e não deve ser capaz de realizar alterações de estrutura em nenhuma tabela.
+CREATE ROLE 'funcionario';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.venda to funcionario;
+GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.cliente to funcionario;
+GRANT SELECT, INSERT, UPDATE, DELETE ON uc4atividades.produto to funcionario;
+
+CREATE USER 'bruna.passos'@'localhost';
+
+GRANT 'funcionario' TO 'bruna.passos'@'localhost';
+
+-- Escolha um método de criptografia ou hash para aplicar às senhas dos usuários. Atualize a tabela “usuário” aplicando a criptografia ou hash ao campo de senha em todos os registros.
+ALTER TABLE usuario MODIFY COLUMN senha VARBINARY(100);
+
+UPDATE usuario 
+SET 
+    senha = AES_ENCRYPT(senha, '@SenhaR00t')
+WHERE id > 0;
+
+SELECT* FROM usuario;
