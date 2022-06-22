fa2c891513140953d3b1fa73d548d3bf6cfc2fef
Victoria Trois <vickytrois@outlook.com>
2022-06-22 20:21:04 -0300

    HTML and JS studies of the day o/
    Learned how to edit a favcon, and headings levels, also deepened my knowledge about the DOM.

---
+++ 3. Java/portugolToJavaExercise/CalculadoraGarcos.java
@@ -0,0 +1,15 @@
+/*
+ * Quando uma empresa contrata o hotel para abrigar eventos, o hotel oferece garçons para servir os convidados.
+ * Cada garçom custa R$ 10,50 por hora. Um dos garçons da equipe coordena os demais e, por isso, recebe R$ 12,00 por hora.
+ * Considerando essas informações, escreva um algoritmo que recebe o número de garçons necessários e o total de horas do evento
+ * e calcule o custo total que o hotel terá com a contratação desses profissionais. Considere que serão informados sempre valores maiores
+ * ou iguais a 1 para as entradas do algoritmo. Ao final, mostre o custo total em tela.
+ *  Exemplo
+ *    Entradas: 4 garçons; 8 horas
+ *    Saída: “Custo total: R$ 348.0”
+ */
+
+float duracaoEvento = 0.00f, hrGarcons = 10.50f, extraGarcomCoord = 1.50f, custoGarcons = 0.00f, custoGarcomCoord = 0.00f, custoTotalEvento = 0.00f;
+int = numGarcons = 0;
+
+System.out.println("Digite a duração do evendo em horas: ");
