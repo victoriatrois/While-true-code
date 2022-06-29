11438f8bec6977fd8302e9ef4192433e720826c3
Victoria Trois <vickytrois@outlook.com>
2022-06-28 23:29:26 -0300

    JS dive

---
+++ 6.JavaScript/cursoEmVideo/moduloD/aula11/ex008.js
@@ -0,0 +1,10 @@
+//para exibir saídas no console com JS usamos console.log
+var velocidade = 66.5;
+
+//condição simples
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h.`);
+if(velocidade > 66){
+  console.log(`MULTADO!`);
+  console.log('Respeite a sinalização de trânsito.');
+}
+
---
+++ 6.JavaScript/cursoEmVideo/moduloD/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = `EUA`;
+console.log(`Vivendo em ${pais},`);
+if(pais == `Brasil`){
+  console.log(`você é brasileire!`);
+} else {
+  console.log(`você é estrangeire!`);
+}
