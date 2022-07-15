f7ef3f37d41e8a7865f2d74f60f73e9a7cf111d6
Victoria Trois <vickytrois@outlook.com>
2022-07-15 13:11:07 -0300

    Practising some while loops in Java

---
+++ 3. Java/TDS-UC06-POO/UC06Atividade01/src/uc06atividade01/UC6Atividade11.java
@@ -0,0 +1,110 @@
+package uc06atividade01;
+
+import java.util.Scanner;
+
+/* 
+ * Escreva um algoritmo que receba o valor normal de uma diária e, em seguida, receba nomes de hóspedes e suas
+ * idades. Caso o hóspede tenha idade inferior a 4 anos, ele não paga hospedagem, nesse caso, mostre na tela
+ * “(Nome do hóspede) possui gratuidade”. Hóspede com mais de 80 anos paga metade, então mostre na tela 
+ * “(Nome do hóspede) paga meia”. O usuário informará hóspedes até digitar a 		 *palavra “PARE”, que interrompe a
+ *  entrada de dados. Ao fim, mostre a quantidade de gratuidades, a quantidade de meias hospedagens e o valor total,
+ *  considerando todos os hóspedes informados.
+ *      Exemplo
+ *              Entrada: R$ 100 a diária
+ *              Entrada: Adolfo, 50 anos
+ *              Entrada: Murilo, 1 ano
+ *              Saída: Murilo possui gratuidade
+ *              Entrada: Joaquim, 82 anos
+ *              Saída: Joaquim paga meia
+ *              Entrada: PARE
+ *              Saída: Total de hospedagens: R$ 150; 1 gratuidade(s); 1 meia(s)
+ */
+
+public class UC6Atividade11 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Qual o valor da diária?");
+    float valorDiaria = in.nextFloat();
+    String strValorDiaria = Float.toString(valorDiaria);
+
+    if (valorDiaria <= 0 || strValorDiaria.indexOf(',') >= 0) {
+      System.out.println("Valor inválido. Use ponto ao invés de vírgula e valores maiores que zero.");
+
+    } else {
+      String pare = "pare";
+      String nome = " ";
+        
+      int contadorGratuidade = 0;
+      int contadorMeia = 0;
+      int contadorInteira = 0;
+
+      while (! nome.equals(pare)) {
+        System.out.println("Qual o nome do hóspede? (Para parar, digite: 'PARE'.");
+        nome = in.next();
+        nome = nome.toLowerCase();
+
+        if (nome.equals(pare)) {
+          break;
+
+        } else {
+          String nomeCap = nome.substring(0, 1).toUpperCase() + nome.substring(1);
+          System.out.println("Qual a idade de " + nomeCap  + "?");
+          int idade = in.nextInt();
+                      
+          if (idade < 4) {
+            System.out.println(nomeCap + " possui gratuidade.");
+            contadorGratuidade ++;
+
+          } else if (idade >= 80) {
+            System.out.println(nomeCap + " paga meia. (" + valorDiaria / 2 + ").");
+            contadorMeia ++;
+
+          } else {
+            System.out.println(nomeCap + " paga inteira. (" + valorDiaria + ").");
+            contadorInteira ++;
+
+          }     
+        }
+      }
+
+      float valorTotal = 0;
+
+      if ((contadorInteira > 0) && (contadorMeia > 0) && (contadorGratuidade > 0)) {
+        valorTotal = valorDiaria * (contadorInteira + (((float) contadorMeia) / 2));
+
+        System.out.println("Valor total em diárias: " + valorTotal + ".");
+        System.out.println("Sendo elas " + contadorInteira + " inteira(s)");
+        System.out.println("           " + contadorMeia + " meia(s) e");
+        System.out.println("           " + contadorGratuidade + " gratuíta(s).");
+
+      } else if ((contadorInteira > 0) && (contadorMeia == 0) && (contadorGratuidade > 0)) {
+        valorTotal = valorDiaria * contadorInteira;
+
+        System.out.println("Valor total em diárias: " + valorTotal + ".");
+        System.out.println("Sendo elas " + contadorInteira + " inteira(s) e");
+        System.out.println("           " + contadorGratuidade + " gratuíta(s).");
+
+      } else if ((contadorInteira > 0) && (contadorMeia > 0) && (contadorGratuidade == 0)) {
+        valorTotal = valorDiaria * (contadorInteira + ((float) contadorMeia / 2));
+
+        System.out.println("Valor total em diárias: " + valorTotal + ".");
+        System.out.println("Sendo elas " + contadorInteira + " inteira(s) e");
+        System.out.println("           " + contadorMeia + " meia(s).");
+
+      } else if ((contadorInteira == 0) && (contadorMeia > 0) && (contadorGratuidade > 0)) {
+        valorTotal = valorDiaria * ((float) contadorMeia/2);
+
+        System.out.println("Valor total em diárias: " + valorTotal + ".");
+        System.out.println("Sendo elas " + contadorMeia + " meia(s) e");
+        System.out.println("           " + contadorGratuidade + " gratuíta(s).");
+
+      } else {
+        valorTotal = valorDiaria * contadorInteira;
+
+        System.out.println("Valor total em diárias: " + valorTotal + ".");
+        
+      }
+    }
+  }
+}
--- 3. Java/portugolToJavaPractice/toJavaExercise/src/atividade02/CalcDiarias04.java
+++ 3. Java/portugolToJavaPractice/toJavaExercise/src/atividade02/CalcDiarias04.java
@@ -21,30 +21,66 @@ public class CalcDiarias04 {
     Scanner in = new Scanner(System.in);
 
     int numQuarto = 1;
+    int precisaDeLimpeza = 0;
+    int maisDiasOcupado = 0;
+    int quartoMaisOcupado = 0;
 
     while (numQuarto > 0) {
       System.out.println("Para interomper a inserção digite '-1'.");
       System.out.println("Digite o número do quarto.");
       numQuarto = in.nextInt();
 
+      String ocupado = "";
+      String limpo = "";
+
       if ((numQuarto < -1 || (numQuarto == 0))){
         System.out.println("Número inválido. Digite um número maior que zero, para interomper a inserção digite '-1'.");
       } else {
-        System.out.println("O quarto " + numQuarto + " está ocupado? Digite 'S' para 'sim' e 'N' para 'não'.");
-        String ocupado = in.next();
-        ocupado = ocupado.toUpperCase();
+        if (numQuarto > 0) {
+          System.out.println("O quarto " + numQuarto + " está ocupado? Digite 'S' para 'sim' e 'N' para 'não'.");
+          ocupado = in.next();
+          ocupado = ocupado.toUpperCase();
 
-        if ((! ocupado.equals("S") || (! ocupado.equals("N")))) {
-          System.out.println("Entrada inválida. Digite 'S' ou 'N'.");
+          if ((! ocupado.equals("S")) && (! ocupado.equals("N"))) {
+            System.out.println("Entrada inválida. Digite 'S' para 'sim' e 'N' para 'não'");
+            break;
 
-        } else {
-          if (ocupado.equals("S")) {
-            System.out.println("O quarto " + numQuarto + " está limpo? Digite 'S' para 'sim' e 'N' para 'não'.");
+          } else {
+            if (ocupado.equals("S")) {
+              System.out.println("O quarto " + numQuarto + " está limpo? Digite 'S' para 'sim' e 'N' para 'não'.");
+              limpo = in.next();
+              limpo = limpo.toUpperCase();
+
+              if ((! limpo.equals("S")) && (! limpo.equals("N"))) {
+                System.out.println("Entrada inválida. Digite 'S' para 'sim' e 'N' para 'não'");
+                break;
+
+              } else {
+                if (limpo.equals("N")){
+                  precisaDeLimpeza ++;
+                }
+
+                System.out.println(precisaDeLimpeza);
+
+                System.out.println("Por quantos dias o quarto " + numQuarto + " ficará ocupado?");
+                int diasOcupado = in.nextInt();
+                  
+                if (diasOcupado > maisDiasOcupado) {
+                  quartoMaisOcupado = numQuarto;
+                  maisDiasOcupado = diasOcupado;
+
+                }
+                }
+                
+            }
           }
         }
-
       }
-
     }
+
+    if (numQuarto == -1) {
+        System.out.println(precisaDeLimpeza + " quarto(s) precisa(m) de limpeza;");
+        System.out.println("O quarto " + quartoMaisOcupado + " ficará mais dias ocupado: " + maisDiasOcupado + ".");
+      }
   }
 }
