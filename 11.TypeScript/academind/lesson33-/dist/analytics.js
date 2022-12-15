239069dd1e4c1ef17e5a4a516fe1f639ab289d4e
Victoria Trois <vickytrois@outlook.com>
2022-12-15 19:58:29 -0300

    Java and TS weekly studies

--- 11.TypeScript/academind/lesson33-/dist/analytics.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let logged; //I can omit types in variables, TS can track it along the code and imply which type it holds.
-function sendAnalytics(data) {
-    console.log(data);
-    logged = true;
-}
-sendAnalytics('The Data');
-//# sourceMappingURL=analytics.js.map
