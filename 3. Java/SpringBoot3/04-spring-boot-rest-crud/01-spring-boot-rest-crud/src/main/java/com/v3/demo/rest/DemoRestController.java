73ce9b7071898a3076dd609522f3cd73927db950
Victoria Trois <vickytrois@outlook.com>
2024-01-08 22:50:34 -0300

    Love2Code | Spring Boot | section 4
    Intro to REST services, REST html and JSON basics.
    Postman Demo.
    JSON to Java POJO with Jackson Data Binding

---
+++ 3. Java/SpringBoot3/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/v3/demo/rest/DemoRestController.java
@@ -0,0 +1,14 @@
+package com.v3.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
